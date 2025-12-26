b3cefe8f0d8eb6feb58b817d2f38e60e6164ecaf
Matteo Golfetto <Matteogol16@gmail.com>
2025-12-26 19:08:31 +0100

    Project files + Manny Import

---
+++ Source/Gauntlet2/Gauntlet2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Gauntlet2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Gauntlet2, "Gauntlet2" );
