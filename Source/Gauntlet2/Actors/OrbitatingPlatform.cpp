cab4a99d66216cb72a60467c6450ea7521d13619
Matteo Golfetto <Matteogol16@gmail.com>
2025-12-30 22:37:22 +0100

    First Prototype Platform

---
+++ Source/Gauntlet2/Actors/OrbitatingPlatform.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OrbitatingPlatform.h"
+
+
+// Sets default values
+AOrbitatingPlatform::AOrbitatingPlatform()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AOrbitatingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	USceneComponent* root = GetRootComponent();
+	
+	
+	root->GetChildrenComponents(false, PlatformsActors);
+	
+	float angle = 360/PlatformsActors.Num();
+	float x = 0;
+	float y = 0;
+	FVector pos = FVector(0,0,0);
+	
+	for (int i = 0; i < PlatformsActors.Num(); i++)
+	{
+		x =  FMath::Sin(angle*i) * Distance;
+		y =  FMath::Cos(angle*i) * Distance;
+		pos.Set(x, y, 0);
+		PlatformsActors[i]->SetRelativeLocation(pos);
+	}
+}
+
+// Called every frame
+void AOrbitatingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	DrawDebugSphere(GetWorld(), GetActorLocation(), 2.f, 10, FColor::Purple);
+}
+
---
+++ Source/Gauntlet2/Actors/OrbitatingPlatform.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "OrbitatingPlatform.generated.h"
+
+UCLASS()
+class GAUNTLET2_API AOrbitatingPlatform : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AOrbitatingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	TArray<USceneComponent*> PlatformsActors;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlatformAnimation")
+	float AnimationSpeed;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlatformAnimation")
+	float Distance;
+	
+	
+	
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
