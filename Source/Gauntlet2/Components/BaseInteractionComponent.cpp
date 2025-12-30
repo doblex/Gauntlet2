7fc22bf2537a5ff462beb1ee0671af70e22dfb62
Matteo Golfetto <Matteogol16@gmail.com>
2025-12-30 22:36:23 +0100

    Interactable and Activable Components

---
+++ Source/Gauntlet2/Components/BaseInteractionComponent.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseInteractionComponent.h"
+
+#include "StaticMeshAttributes.h"
+#include "Gauntlet2/Lib/BFL_Miscellaneous.h"
+
+
+// Sets default values for this component's properties
+UBaseInteractionComponent::UBaseInteractionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UBaseInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+	DynamicMaterialInstance = UMaterialInstanceDynamic::Create(ParentMsaterial, this);
+	
+	if (DynamicMaterialInstance)
+	{
+		DynamicMaterialInstance->SetVectorParameterValue("Color", BaseColor);
+	}
+	
+	
+	TArray<UStaticMeshComponent*> StaticMeshes;
+	
+	UBFL_Miscellaneous::CollectStaticMeshComponents(GetOwner()->GetRootComponent(), StaticMeshes);
+	
+	for (auto Component : StaticMeshes)
+	{
+		Component->SetMaterial(0, DynamicMaterialInstance);
+	}
+	
+}
+
+
+// Called every frame
+void UBaseInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                          FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UBaseInteractionComponent::Interact_Implementation()
+{
+	IInteractable::Interact_Implementation();
+	
+	bIsActive = !bIsActive;
+	
+	if (DynamicMaterialInstance)
+	{
+		DynamicMaterialInstance->SetVectorParameterValue("Color",  bIsActive ? InteractColor : BaseColor);
+	}
+}
+
---
+++ Source/Gauntlet2/Components/BaseInteractionComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Gauntlet2/Interfaces/Interactable.h"
+#include "BaseInteractionComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GAUNTLET2_API UBaseInteractionComponent : public UActorComponent , public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UBaseInteractionComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	bool bIsActive;
+	
+	UMaterialInstanceDynamic* DynamicMaterialInstance;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Component")
+	UMaterialInterface* ParentMsaterial;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Component")
+	FColor BaseColor;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Component")
+	FColor InteractColor;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+	
+	virtual void Interact_Implementation() override;
+};
